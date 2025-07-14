15084c677b0717c97f1478712b9c790c1efb60d9
NimeshGhag <ghagnimesh490@gmail.com>
2025-07-14 12:47:34 +0530

    data added through array of object

---
+++ js/services/songServices.js
@@ -0,0 +1,70 @@
+const playlist = [
+    {
+        title: "Senorita",
+        img: "../assets/images/music poster/Zindagi-Na-Milegi-Dobara-2011-500x500.webp",
+        url: "../assets/music/Senorita Zindagi Na Milegi Dobara Full HD Video Song  Farhan Akhtar, Hrithik Roshan, Abhay Deol - T-Series.mp3",
+    },
+
+    {
+        title: "shape of you",
+        img: "../assets/images/music poster/ed Sheeran.webp",
+        url: "../assets/music/Ed Sheeran - Shape of You (Lyrics) - 7clouds.mp3",
+    },
+
+    {
+        title: "Gallan Goodiyaan'",
+        img: "../assets/images/music poster/dill dhadkane do.webp",
+        url: "../assets/music/'Gallan Goodiyaan' Full VIDEO Song  Dil Dhadakne Do  T-Series - T-Series.mp3",
+    },
+
+    {
+        title: "sajni re",
+        img: "../assets/images/music poster/sajni re.webp",
+        url: "../assets/music/Sajni (Lyrical Video) Arijit Singh, Ram Sampath  Laapataa Ladies   Aamir Khan Productions - T-Series.mp3",
+    },
+
+    {
+        title: "Kashmir Main Tu Kanyakumari",
+        img: "../assets/images/music poster/Chennai_Express-Kashmir-Main-Tu-Kanyakumari.webp",
+        url: "../assets/music/Kashmir Main Tu Kanyakumari(Lyrical)Chennai Express Shahrukh K, Deepika P,Sunidhi C,Arijit S,Neeti - T-Series.mp3",
+    },
+
+    {
+        title: "Malhari",
+        img: "../assets/images/music poster/malhari.webp",
+        url: "../assets/music/Malhari Full Video Song  Bajirao Mastani  Ranveer Singh - Eros Now Music.mp3",
+    },
+
+    {
+        title: "Zingaat",
+        img: "../assets/images/music poster/sairat.webp",
+        url: "../assets/music/Zingaat - Sairat  Official Full Video with English subtitles  Nagraj Manjule  Ajay Atul - Zee Music Marathi.mp3",
+    },
+
+    {
+        title: "Naacho Naacho",
+        img: "../assets/images/music poster/nacho nacho.webp",
+        url: "../assets/music/Naacho Naacho (Full Video) RRR - NTR, Ram Charan  M M Kreem  SS Rajamouli  Vishal Mishra & Rahul - T-Series.mp3",
+    },
+
+    {
+        title: "Namo Namo",
+        img: "../assets/images/music poster/namo-namo-kedarnath.webp",
+        url: "../assets/music/Namo Namo - Lyrical  Kedarnath  Sushant Rajput  Sara Ali Khan  Amit Trivedi  Amitabh B - Zee Music Company.mp3",
+    },
+
+    {
+        title: "Kajra Re",
+        img: "../assets/images/music poster/kajra re.webp",
+        url: "../assets/music/Kajra Re  Full Song  Bunty Aur Babli  Aishwarya, Abhishek, Amitabh Bachchan  Shankar-Ehsaan-Loy - YRF.mp3",
+    },
+
+    {
+        title: "BESABRIYAAN",
+        img: "../assets/images/music poster/M-S-Dhoni-The-Untold-Story-3-Hindi-2016-500x500.webp",
+        url: "../assets/music/BESABRIYAAN Full Video Song  M. S. DHONI - THE UNTOLD STORY  Sushant Singh Rajput - T-Series.mp3",
+    },
+];
+
+ 
+
