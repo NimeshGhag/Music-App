441b3bfeb5585ad3ffb22a969e2d943bef1389bb
NimeshGhag <ghagnimesh490@gmail.com>
2025-07-18 14:06:11 +0530

    next song functionlity done

--- js/player/mini-player.js
+++ js/player/mini-player.js
@@ -1,15 +1,20 @@
-import { audio } from "./audio.js";
+import { audio, currentSongIndex, playSong } from "./audio.js";
+import { playlist } from "../services/songServices.js";
 
 export function updateMiniPlayer(song) {
     document.querySelector(".track img").src = song.img;
     document.querySelector(".track-name h3").textContent = song.title;
-    document.querySelector(".track-name small").textContent = song.artist.slice(0, 10)
+    document.querySelector(".track-name small").textContent = song.artist.slice(
+        0,
+        10
+    );
     document.querySelector("#mini-play-button i").className =
         "ri-pause-mini-fill";
 }
 
 export function setUpMiniPlayerControls() {
     const playBtn = document.getElementById("mini-play-button");
+    const nextBtn = document.querySelector("#next-button");
 
     playBtn.addEventListener("click", () => {
         const icon = playBtn.querySelector("i");
@@ -21,6 +26,15 @@ export function setUpMiniPlayerControls() {
         } else {
             audio.pause();
             icon.className = "ri-play-mini-fill";
+        }
+    });
+
+    nextBtn.addEventListener("click", () => {
+        let nextIndex = (currentSongIndex + 1) % playlist.length;
+        let nextSong = playlist[nextIndex]
+        if (nextSong) {
+            playSong(nextSong, nextIndex);
+            updateMiniPlayer(nextSong);
         }
     });
 }
